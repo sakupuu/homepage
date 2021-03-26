ab80fab0ed6e20e95658a74197fdb0260a2564ce
sakupuu <saku.yuzu.113@gmail.com>
2021-03-26 23:30:27 +0900

    クッキーを追加しました

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
-console.log('テストです');
+/* document.getElementById('choice').textContent = new Date(); */
+// console.log(window.confirm('このページを開きますか？'));
+<script src="../../_common/scripts/js.cookie/js"></script>
+const agree = Cookies.get('cookie-agree');
+if(agree === 'yes') {
+  console.log('クッキーを確認しました');
+} else {
+  console.log('クッキーを確認できません');
+  document.getElementById('agreebtn').onclick = function() {
+    Cookies.set('cookie-agree', 'yes', {expires: 7});
+  };
+}
