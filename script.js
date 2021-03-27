dfb6c4b60903fb991bf213907cb18e641d39c477
sakupuu <saku.yuzu.113@gmail.com>
2021-03-27 23:56:02 +0900

    途中保存です

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-'use strict';
+// 'use strict';
 /* document.getElementById('choice').textContent = new Date(); */
 // console.log(window.confirm('このページを開きますか？'));
-<script src="../../_common/scripts/js.cookie/js"></script>
-const agree = Cookies.get('cookie-agree');
-if(agree === 'yes') {
-  console.log('クッキーを確認しました');
-} else {
-  console.log('クッキーを確認できません');
-  document.getElementById('agreebtn').onclick = function() {
-    Cookies.set('cookie-agree', 'yes', {expires: 7});
-  };
-}
+
+// const agree = Cookies.get('cookie-agree');
+// const panel = document.getElementById('privacy-panel');
+// if(agree === 'yes') {
+//   document.body.removeChild(panel);
+// } else {
+//   document.getElementById('agreebtn').onclick = function() {
+//     Cookies.set('cookie-agree', 'yes', {expires: 7});
+//     document.body.removeChild(panel);
+//   };
+// }
+// // クッキー削除（テスト用）
+// document.getElementById('testbtn').onclick = function() {
+//   Cookies.remove('cookie-agree');
+// };
